f85aea0efaf50e67d56823dcf47439e1ad3c2a51
quaint <twisnia@gmail.com>
2010-11-22 09:31:02 +0100

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+describe "LayoutLinks" do
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
@@ -18,5 +20,10 @@ require 'spec_helper'
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
   end
 end
