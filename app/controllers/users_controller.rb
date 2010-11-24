2a0f3fa58a59ebffa75993e8724a26dc6ce40227
quaint <twisnia@gmail.com>
2010-11-24 15:00:46 +0100

    Added user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,6 +55,20 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def deny_for_signedin
